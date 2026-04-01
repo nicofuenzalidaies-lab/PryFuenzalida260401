bfda64f312667c1cce6089b0858b455f5905fdca
Alumno <Alumno@PC143>
2026-04-01 09:26:57 -0300

    Agregar archivos de proyecto.

---
+++ PryFuenzalida260401/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace PryFuenzalida260401
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.lblproducto = new System.Windows.Forms.Label();
+            this.lblcodigo = new System.Windows.Forms.Label();
+            this.lblcantidad = new System.Windows.Forms.Label();
+            this.lblstock = new System.Windows.Forms.Label();
+            this.lblprecio = new System.Windows.Forms.Label();
+            this.lbldescripcion = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.lblcategoria = new System.Windows.Forms.Label();
+            this.cbocategoria = new System.Windows.Forms.ComboBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.gpbtodo = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.gpbtodo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(126, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(105, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(0, 118);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(104, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(126, 118);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(105, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(253, 118);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(116, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // lblproducto
+            // 
+            this.lblproducto.AutoSize = true;
+            this.lblproducto.Location = new System.Drawing.Point(18, 11);
+            this.lblproducto.Name = "lblproducto";
+            this.lblproducto.Size = new System.Drawing.Size(50, 13);
+            this.lblproducto.TabIndex = 6;
+            this.lblproducto.Text = "Producto";
+            this.lblproducto.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblcodigo
+            // 
+            this.lblcodigo.AutoSize = true;
+            this.lblcodigo.Location = new System.Drawing.Point(148, 11);
+            this.lblcodigo.Name = "lblcodigo";
+            this.lblcodigo.Size = new System.Drawing.Size(40, 13);
+            this.lblcodigo.TabIndex = 7;
+            this.lblcodigo.Text = "Codigo";
+            // 
+            // lblcantidad
+            // 
+            this.lblcantidad.AutoSize = true;
+            this.lblcantidad.Location = new System.Drawing.Point(279, 11);
+            this.lblcantidad.Name = "lblcantidad";
+            this.lblcantidad.Size = new System.Drawing.Size(49, 13);
+            this.lblcantidad.TabIndex = 8;
+            this.lblcantidad.Text = "Cantidad";
+            // 
+            // lblstock
+            // 
+            this.lblstock.AutoSize = true;
+            this.lblstock.Location = new System.Drawing.Point(33, 85);
+            this.lblstock.Name = "lblstock";
+            this.lblstock.Size = new System.Drawing.Size(35, 13);
+            this.lblstock.TabIndex = 9;
+            this.lblstock.Text = "Stock";
+            // 
+            // lblprecio
+            // 
+            this.lblprecio.AutoSize = true;
+            this.lblprecio.Location = new System.Drawing.Point(148, 85);
+            this.lblprecio.Name = "lblprecio";
+            this.lblprecio.Size = new System.Drawing.Size(37, 13);
+            this.lblprecio.TabIndex = 10;
+            this.lblprecio.Text = "Precio";
+            // 
+            // lbldescripcion
+            // 
+            this.lbldescripcion.AutoSize = true;
+            this.lbldescripcion.Location = new System.Drawing.Point(265, 85);
+            this.lbldescripcion.Name = "lbldescripcion";
+            this.lbldescripcion.Size = new System.Drawing.Size(63, 13);
+            this.lbldescripcion.TabIndex = 11;
+            this.lbldescripcion.Text = "Descripcion";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(263, 39);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(85, 20);
+            this.numericUpDown1.TabIndex = 12;
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.Location = new System.Drawing.Point(152, 246);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(91, 26);
+            this.btnagregar.TabIndex = 13;
+            this.btnagregar.Text = "Agregar";
+            this.btnagregar.UseVisualStyleBackColor = true;
+            // 
+            // lblcategoria
+            // 
+            this.lblcategoria.AutoSize = true;
+            this.lblcategoria.Location = new System.Drawing.Point(26, 13);
+            this.lblcategoria.Name = "lblcategoria";
+            this.lblcategoria.Size = new System.Drawing.Size(52, 13);
+            this.lblcategoria.TabIndex = 14;
+            this.lblcategoria.Text = "Categoria";
+            // 
+            // cbocategoria
+            // 
+            this.cbocategoria.FormattingEnabled = true;
+            this.cbocategoria.Location = new System.Drawing.Point(12, 39);
+            this.cbocategoria.Name = "cbocategoria";
+            this.cbocategoria.Size = new System.Drawing.Size(103, 21);
+            this.cbocategoria.TabIndex = 15;
+            // 
+            // gpbtodo
+            // 
+            this.gpbtodo.Controls.Add(this.numericUpDown1);
+            this.gpbtodo.Controls.Add(this.lbldescripcion);
+            this.gpbtodo.Controls.Add(this.lblprecio);
+            this.gpbtodo.Controls.Add(this.lblstock);
+            this.gpbtodo.Controls.Add(this.lblcantidad);
+            this.gpbtodo.Controls.Add(this.lblcodigo);
+            this.gpbtodo.Controls.Add(this.lblproducto);
+            this.gpbtodo.Controls.Add(this.textBox6);
+            this.gpbtodo.Controls.Add(this.textBox5);
+            this.gpbtodo.Controls.Add(this.textBox4);
+            this.gpbtodo.Controls.Add(this.textBox2);
+            this.gpbtodo.Controls.Add(this.textBox1);
+            this.gpbtodo.Location = new System.Drawing.Point(12, 80);
+            this.gpbtodo.Name = "gpbtodo";
+            this.gpbtodo.Size = new System.Drawing.Size(372, 160);
+            this.gpbtodo.TabIndex = 16;
+            this.gpbtodo.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(416, 320);
+            this.Controls.Add(this.gpbtodo);
+            this.Controls.Add(this.cbocategoria);
+            this.Controls.Add(this.lblcategoria);
+            this.Controls.Add(this.btnagregar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.gpbtodo.ResumeLayout(false);
+            this.gpbtodo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label lblproducto;
+        private System.Windows.Forms.Label lblcodigo;
+        private System.Windows.Forms.Label lblcantidad;
+        private System.Windows.Forms.Label lblstock;
+        private System.Windows.Forms.Label lblprecio;
+        private System.Windows.Forms.Label lbldescripcion;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btnagregar;
+        private System.Windows.Forms.Label lblcategoria;
+        private System.Windows.Forms.ComboBox cbocategoria;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.GroupBox gpbtodo;
+    }
+}
+
---
+++ PryFuenzalida260401/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("PryFuenzalida260401")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PryFuenzalida260401")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("c29d85aa-0dea-4fb9-8fac-a4072477a236")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
